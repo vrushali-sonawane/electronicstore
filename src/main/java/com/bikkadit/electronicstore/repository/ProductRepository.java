18f23bf7474871cd24e7a473780f0c6b7f175e81
vrushali-sonawane <svrushali7997@gmail.com>
2023-07-23 13:48:59 +0530

    product model complete upto productRepository

---
+++ src/main/java/com/bikkadit/electronicstore/repository/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.bikkadit.electronicstore.repository;
+
+import com.bikkadit.electronicstore.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,String> {
+    //search
+
+
+    Page<Product> findByTitleContaining(String subTitle,Pageable pageable);
+
+    Page<Product> findByLiveTrue(Pageable pageable);
+    //other method
+    //custom finder method
+    //query method
+
+}
