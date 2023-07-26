a9db4e1e1d3ccb01946ebddd1b48935f61678b78
vrushali-sonawane <svrushali7997@gmail.com>
2023-07-26 11:58:49 +0530

    use of App Constant in product model

--- src/main/java/com/bikkadit/electronicstore/help/AppConstant.java
+++ src/main/java/com/bikkadit/electronicstore/help/AppConstant.java
@@ -15,6 +15,8 @@ public class AppConstant {
     public static final String USER_NOT_FOUND="user not found with this userId ";
     public static final String NOT_FOUND="user not found with this emailId ";
     public static final String CATEGORY_NOT_FOUND="category not found with this categoryId ";
+    public static final String DELETE_PRODUCT="product deleted successfully";
+    public static final String PRODUCT_NOT_FOUND="product not found with productId";
 
 
 
